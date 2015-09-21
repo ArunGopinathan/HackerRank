14e56f40418f31d3ad235445d08c39b7fba457e4
Arun G <90arun.g@gmail.com>
2015-09-21 01:57:30 -0500

    Java - Big Number - Hackerrank
    Solutions to Java Big Number Hackerrank
    https://www.hackerrank.com/domains/java/bignumber

---
+++ Java/BigNumber/JavaBigInteger.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
+        
+        sc.close();
+    }
+}
---
+++ Java/BigNumber/readme.txt
@@ -0,0 +1,2 @@
+Solutions to Java Big Number Hackerrank problems
+https://www.hackerrank.com/domains/java/bignumber
